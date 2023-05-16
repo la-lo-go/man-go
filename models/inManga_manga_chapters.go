2311f3c90cdc8891ab0dd7ef59e7a1b92e317c14
Lalo <86909609+la-lo-go@users.noreply.github.com>
2023-05-17 01:54:59 +0200

    [ADD] InManga types

---
+++ models/inManga_manga_chapters.go
@@ -0,0 +1,30 @@
+package inManga
+
+import (
+	"gorm.io/gorm"
+)
+
+type InMangaManga struct {
+	gorm.Model
+
+	MangaName string           `json:"mangaName"`
+	Chapters  []InMangaChapter `json:"chapters"`
+}
+
+type InMangaChapter struct {
+	gorm.Model
+
+	Number         float64       `json:"number"`
+	PagesCount     int           `json:"pagesCount"`
+	ID             string        `json:"id"`
+	Pages          []InMangaPage `json:"pages"`
+	InMangaMangaID int           `json:"inMangaMangaID"`
+}
+
+type InMangaPage struct {
+	gorm.Model
+
+	Number           int    `json:"number"`
+	ID               string `json:"id"`
+	InMangaChapterID int    `json:"inMangaChapterID"`
+}
